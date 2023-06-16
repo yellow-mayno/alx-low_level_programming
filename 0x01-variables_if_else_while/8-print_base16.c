ee17e02ce15c78691713071719b4219a6c63f461
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-17 00:38:23 +0200

    r

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - dsfsdfsdsf
+ *
+ * Return: 0 dsfsdfsdsfsdfsd;df
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0x30; i <= 0x46; i++)
+	{
+		if (i < 0x3A || i > 0x40)
+			putchar(i);
+	}
+	putchar(0xA);
+	return (0);
+}

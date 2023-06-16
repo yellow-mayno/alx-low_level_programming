dc467fd6563e38505e6e3809d253af4ab9375eb4
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-16 18:44:28 +0200

    r

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/*
+ * main - dsfdsfdsfdfdsfgdsfgdsfg
+ *
+ * Description: sdfsdfsdfsdfsdfsdfsd
+ * Return: 0
+ */
+
+/**
+ * main -dsfsfdds sdf
+ *
+ * Return: 0dsfsdfsdf
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+
+	a = sizeof(char);
+	b = sizeof(int);
+	c = sizeof(long int);
+	d = sizeof(long long int);
+	e = sizeof(float);
+	printf("Size of a char: %d byte(s)\n", a);
+	printf("Size of a int: %d byte(s)\n", b);
+	printf("Size of a long int: %d byte(s)\n", c);
+	printf("Size of a long long int: %d byte(s)\n", d);
+	printf("Size if a float: %d byte(s)\n", e);
+	return (0);
+}

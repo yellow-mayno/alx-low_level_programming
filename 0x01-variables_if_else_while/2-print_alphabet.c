7da5335c0f55e5964f0b2c887f461fd225fbb47d
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-16 23:15:50 +0200

    r

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - sdfsqdfsqdf
+ *
+ * Return: 0 dsfsdfsdf
+*/
+int main(void)
+{
+	int a;
+	char ch;
+
+	for (a = 0x61 ; a < 0x7b  ; a++)
+	{
+		ch = (char)a;
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - sdfsqdfsqdf
+ *
+ * Return: 0 dsfsdfsdf
+*/
+int main(void)
+{
+	int a;
+	char ch;
+
+	for (ch='a' ; ch <= 'z' ; ch++)                                                                         {                                                                                                               putchar(ch);                                                                                    }  
+	putchar('\n');
+	return (0);
+}
+

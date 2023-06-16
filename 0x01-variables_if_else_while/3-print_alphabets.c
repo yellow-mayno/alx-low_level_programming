de2521d95739091ccc1760357bbbbeb69eeb53fc
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-16 23:26:40 +0200

    r

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - sdfsqdfsqdf
+ *
+ * Return: 0 dsfsdfsdf
+*/
+int main(void)
+{
+	int a;
+	char ch;
+
+	for (a = 0x61 ; a < 0x7b  ; a++)
+	{
+		ch = (char)a;
+		putchar(ch);
+	}
+	for (a = "A" ; a <= "Z" ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}

47cd0fac3ea47957e9d96f1ad78ff283d444ce1e
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-17 00:04:14 +0200

    r

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - sdfsdfssd
+ *
+ * Return: 0 dsfsdfsdfqsfd
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0x30; i < 0x3A; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}

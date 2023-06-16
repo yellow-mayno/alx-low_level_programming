bdca40a94095ad9a9c11004f086b57d0819ffb1d
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-16 23:52:28 +0200

    r

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - sdfsdfs
+ *
+ * Return: 0 sdfsdsdsd
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}

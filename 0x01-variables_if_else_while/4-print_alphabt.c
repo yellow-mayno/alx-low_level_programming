2a645f47d8483a28a5f10d9a5aa8cf415fd72ec3
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-16 23:43:59 +0200

    r

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - dsjfshdkjfsh
+ *
+ * Return: 0 4654444545
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0x61; a < 0x7b; a++)
+	{
+		if (a != 0x65 && a != 0x71)
+			putchar(a);
+	}
+	return (0);
+}

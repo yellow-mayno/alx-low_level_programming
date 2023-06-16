3b01adc1c388c37b949729d63bf1474bfbf19b36
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-17 00:09:52 +0200

    r

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - sdfsqdfsqdf
+ *
+ * Return: 0 dsfsdfsdf
+*/
+int main(void)
+{
+	int a;
+	char ch;
+
+	for (a = 0x7a ; a >= 0x61  ; a--)
+	{
+		ch = (char)a;
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}

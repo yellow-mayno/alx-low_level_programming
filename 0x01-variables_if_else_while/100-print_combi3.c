ec30d1f51a2dc1318641cf2ebe72071f34122b6e
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-17 09:20:44 +0200

    r

---
+++ 0x01-variables_if_else_while/100-print_combi3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - sdfsdfsdf
+ *
+ * Return: 0 dsfsdfs
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+
+		if (i == 56)
+			break;
+		putchar(44);
+		putchar(32);
+		}
+	}
+	putchar(10);
+	return (0);
+}

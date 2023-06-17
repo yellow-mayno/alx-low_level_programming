2dc65df02aaa9c276ca4a1283243f98f332e89ef
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-17 18:52:19 +0200

    r

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main -sdfsdfs
+ *
+ * Return: 0 dsfsdfsfds
+ */
+void main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			l = j;
+			for (k = i; k < 10; k++)
+			{
+				for (; l < 10; l++)
+				{
+					putchar(i + 48);
+					putchar(j + 48);
+					putchar(32);
+					putchar(k + 48);
+					putchar(l + 48);
+					if (i == 9 && j == 8 && l == 9)
+					{
+						putchar(10);
+						break;
+					}
+					putchar(44);
+					putchar(32);
+				}
+				l = 0;
+			}
+		}
+	}
+}

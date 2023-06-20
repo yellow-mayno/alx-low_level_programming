ccb4c3943c4060ed264f5a55ed1fa8b076a378b4
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-20 22:24:19 +0200

    r

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - dsfsdfsd
+ *
+ * Return: 0fddsfd
+ */ 
+void print_alphabetx10(void)
+{
+	int i; 
+	int n;
+
+	for (n = 0; i < 10; i++)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+			_putchar(10);
+		}
+	}
+}			
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,2 @@
 void print_alphabet(void);
+void print_alphabetx10(void);

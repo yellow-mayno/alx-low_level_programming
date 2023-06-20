5d1db4445f50fa2e2a54f8023d5b8790e99faee7
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-20 18:07:12 +0200

    r

---
+++ 0x02-function_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+/**
+ * main - sdfsdf
+ *
+ * Return: 0sdfsdfsdf
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-function_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - qsdsd
+ *
+ * Return: 0fddsf
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return;
+}

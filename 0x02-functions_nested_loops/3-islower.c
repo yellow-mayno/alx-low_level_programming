e062fc95001d6785344f3822466fcd9c0078e3cc
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-20 22:57:06 +0200

    r

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - dsfdsfsd
+ * @c: dsfsjhdgfsjhdgfjsdhf
+ *
+ * Return: 0sdjfsjdfnsjd
+ */
+int _islower(int c)
+{
+	if (i < 123 && i > 96)
+		return (1);
+	else
+		return (0)
+}

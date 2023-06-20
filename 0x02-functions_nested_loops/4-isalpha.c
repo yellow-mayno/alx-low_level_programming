f5bc5e3a70396c53654adfff76b50f70a7477c58
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-20 23:38:42 +0200

    r

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - dsfsdfsdfs
+ * @c: dsfsdfsdf
+ *
+ * Return: 0dkjfdkfjg
+ */ 
+int _isalpha(int c)
+{
+	if (c > 64 && c < 91 && c < 123 && c > 96)
+		return (1);
+	else
+		return (0);
+}

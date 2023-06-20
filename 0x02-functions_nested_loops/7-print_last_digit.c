947a022d18e9e21dbaeea3cca64df613e3e807d7
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-21 01:37:13 +0200

    r

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - sdfsdfsdfsd
+ * @z : sdfsdfsdf
+ *
+ * Return: 0 dsfs
+ */
+int print_last_digit(int z)
+{
+	int ld;
+
+	ld = z % 10;
+	_putchar('0' + ld);
+	return (z);
+}

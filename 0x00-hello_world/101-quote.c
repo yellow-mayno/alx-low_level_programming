d47bbce1afb681aabd60f60240cf0a5cb60868f8
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-21 16:51:21 +0200

    r

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,11 +8,12 @@
 int main(void)
 {
 	int i;
-	char message[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char message[59]
+			 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	for (i = 0; i < 59; i++)
 	{
 		putchar(message[i]);
 	}
-	return (0);
+	return (1);
 }

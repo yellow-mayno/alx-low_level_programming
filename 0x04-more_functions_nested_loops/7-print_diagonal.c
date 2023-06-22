7d2845480e7b4fd867b4b4c51b47d69035ae35a1
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-22 23:09:21 +0200

    r

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_diagonal - fdjdsnfkjdskfjsd
+* @mok: sdfsdfsdf I hate betty and her *&/&**!! description
+* Return: dssdfsd
+*/
+void print_diagonal(int mok)
+{
+	int i, j;
+
+	for (i = 0; i < mok; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(0x20);
+		_putchar('\\');
+		_putchar(0xA);
+	}
+}

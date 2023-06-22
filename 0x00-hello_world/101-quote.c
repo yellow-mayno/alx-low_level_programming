f97100639fe33011dd8a2bd46d4f7cc6810e4e25
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-22 21:53:53 +0200

    r

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 	char message[59]
-			 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	for (i = 0; i < 59; i++)
 	{
--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,6 +10,5 @@ int main(void)
 
 	i = 98;
 	positive_or_negative(i);
-
 	return (0);
 }

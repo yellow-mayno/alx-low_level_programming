161838ec1f3b6e13a42bcd76d6499f0f057db80e
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-22 22:58:31 +0200

    r

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_line - dsfsqdfsqdfsqdfsqfdqsdfsqfd
+* @n : fdsjhwsikjhgfkjdsfhgdskjhfgds
+* Return: waluuu
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar(95);
+	_putchar(10);
+}

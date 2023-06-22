063f162d8c86d3d3de93680dec78c4f0989029f8
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-22 20:35:59 +0200

    r

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "2-largest_number.c"
+/**
+* main - sdfsd
+*
+* Return: fddsfds
+*/
+void main(void)
+{
+	int a, b, c;
+	int largest;
+
+	a = 972;
+	b = -20;
+	c = 0;
+	largest = largest_number(a, b, c);
+	printf("%d\n", largest);
+}

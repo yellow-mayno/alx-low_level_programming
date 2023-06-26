65e3a72595f776f4e62b9d82b801300b84a91adc
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-26 21:40:33 +0200

    r

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+/**
+* _strlen - sdfsdfs
+* @s: dsfsdfs
+* Return: intertdfvdfvdfg
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
---
+++ 0x05-pointers_arrays_strings/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	char hell[]={'H','E','L','L'};
+	int i;
+;
+for (i=0; i<10;i++) 
+	printf("%c\n", hell[i]);
+}	

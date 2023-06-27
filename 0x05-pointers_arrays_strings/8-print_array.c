3f68746e558fac8a23e1ae6894da85f1806d61cf
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-27 22:40:52 +0200

    r

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* print_array - dsfsdf
+* @a: dfgdfg
+* @n: fgdfgd
+* Return: gfhfg
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	printf("%d\n", a[0]);
+	for (i = 1; i < n; i++)
+		printf(", %d\n", a[i]);
+	putchar(10);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,3 +7,4 @@ void _putchar(char c);
 void rev_string(char *s);
 void puts2(char *s);
 void puts_half(char *s);
+void print_array(int *a, int n);

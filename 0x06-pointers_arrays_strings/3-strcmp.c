4b2de9908d2adcf624086289d3125efc2a026c29
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-30 20:58:24 +0200

    r

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0;
+	int i = 0;
 
 	while (s1[i] != 0)
 	{
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
 char *_strcat(char *dest, char *f);
 char *_strncat(char *f, char *j, int oi);
 char*_strncpy(char *de, char *s, int n);
-int _stcmp(char*, char*);
+int _strcmp(char*, char*);

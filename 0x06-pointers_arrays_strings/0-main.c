9d99c4ee43e8fde91fdaacbbdf0672c081d1e3f8
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-06-30 12:24:59 +0200

    blabla

---
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - dsfsd
+* Return: dfdf
+*/
+int main(void)
+{
+	char dest[] = "It's so";
+	char src[] = " fkn over !\n";
+
+	printf("%s\n", dest);
+	_strcat(dest, src);
+	printf("%s\n", dest);
+	return (0);
+}

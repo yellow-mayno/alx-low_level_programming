572bce83df5019be39ab51428646d5fbadc1af23
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-01 18:50:43 +0200

    r

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,47 @@
+int is_in(char *, char);
+/**
+* cap_string - dfggfd
+* @s: sdfsd
+* Return: dsfsdf
+*/
+char *cap_string(char *s)
+{
+	int c, i = 1;
+	char separator[]
+		 = {',', ';', '.', '!', '?', '\"', '(', ')', '{', '}', 9, 10, 32, '\0'};
+
+	c = is_in(separator, s[0]);
+	while (s[i] != '\0')
+	{
+		if (s[i] < 123 && s[i] > 96 && c)
+		{
+			s[i] = s[i] - 32;
+			c = 0;
+		}
+		else if ((s[i] > 47 && s[i] < 58) || (s[i] > 64 && s[i] < 91))
+			c = 0;
+		else
+			c = c + is_in(separator, s[i]);
+		i++;
+	}
+	return (s);
+}
+
+/**
+* is_in - search if there is c in s
+* @c: character
+* @s: string
+* Return: 0 or 1
+*/
+int is_in(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,4 +3,6 @@ char *_strncat(char *f, char *j, int oi);
 char*_strncpy(char *de, char *s, int n);
 int _strcmp(char*, char*);
 void reverse_array(int *, int);
-char *stringç_toupper(char *);
+char *string_toupper(char *);
+int is_in(char *, char);
+char *cap_string(char *);

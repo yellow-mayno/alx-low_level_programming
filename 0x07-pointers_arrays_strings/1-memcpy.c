432241cc4695b848f3effd9b46300d467b1fa0ab
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-03 20:29:39 +0200

    r

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+/**
+* _memcpy - dsfsfd
+* @dest: sdfsdf
+* @src: sdfs
+* @n: sdfsdf
+* Return: dsfsdf
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}

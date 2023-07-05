e804028d12ed0213fbee9fc55ab216cd4d023ee5
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-05 14:22:15 +0200

    r

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+/**
+* factorial - sdfsdf
+* @n: dfgdf
+* Return: intdfgdfg
+*/
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
---
+++ 0x08-recursion/3-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+int factorial(int n)
+{
+	int neg = -10;
+	int nada = 0;
+	int pos = 5;
+
+	printf("%d , %d, %d\n", factorial(neg), factorial(nada), 
+																																																																																			factorial(pos));
+	return (0);
+}

d5263255eb6de4deb694a99d15aef9e2cd5b6117
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-05 18:00:32 +0200

    r

--- 0x08-recursion/5-main.c
+++ 0x08-recursion/5-main.c
@@ -20,7 +20,7 @@ int main(void)
     printf("%d\n", r);
     r = _sqrt_recursion(25);
     printf("%d\n", r);
-    r = _sqrt_recursion(-1);
+    r = _sqrt_recursion(-16);
     printf("%d\n", r);
     return (0);
 }

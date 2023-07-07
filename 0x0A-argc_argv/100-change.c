b6732d5bc3e5240cbc92be16c5af1a979345bb26
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-08 01:37:44 +0200

    r

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - dsfsdf
+* @argv: sdfsdf
+* @argc: sdfdsf
+* Return: sdfdsf
+*/
+int main(int argc, char *argv[])
+{
+	int i = atoi(argv[1]);
+	int sum;
+
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+	if (i <= 0)
+	{
+		puts("0");
+		return (0);
+	}
+	sum = (i / 25) + ((i % 25) / 10) + (((i % 25) % 10) / 5) +
+		 ((((i % 25) % 10) % 5) / 2) + ((((i % 25) % 10) % 5) % 2);
+	printf("%d\n", sum);
+	return (0);
+}

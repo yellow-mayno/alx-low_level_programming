004e9aa3124759c36f8a5ed69bb56c8ca2c292c4
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-07 20:34:57 +0200

    r

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - sdfsdf
+* @argv: sdfsdf
+* @argc: sdfsdf
+* Return: 0
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}

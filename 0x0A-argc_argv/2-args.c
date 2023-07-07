ebae2a59579536342c1de171e6facfd4ac5818e2
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-07 20:51:17 +0200

    r

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - sdfsdf
+* @argc: sdfsdf
+* @argv: sdfsdfs
+* Return: sdfsdf
+*/
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}

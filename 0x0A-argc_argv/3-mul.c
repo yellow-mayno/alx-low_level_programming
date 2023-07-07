d921b20d6ec5d42300f0e0cb424f2e8f343e0c08
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-07 22:16:53 +0200

    r

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+int atoi(char *);
 /**
 * main - dsfsd
 * @argv: sdfsdf
@@ -33,4 +34,27 @@ int main(int argc, char *argv[])
 	mul = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", mul);
 	return (mul);
+}
+
+/**
+* atoi - sdfsdfsd
+* @s: sdfsdf
+* Return: dsfgdfg
+*/
+int atoi(char *s)
+{
+	int sum = 0;
+	int sign = 1;
+	int i = 1;
+
+	if (*s == '-')
+		sign = -1;
+	else
+		i--;
+	while (s[i] != '\0')
+	{
+		sum = sum * 10 + s[i] - 48;
+		i++;
+	}
+	return (sum * sign);
 }

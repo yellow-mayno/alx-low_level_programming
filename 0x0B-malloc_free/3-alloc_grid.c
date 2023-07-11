4a2a203ed33bf59587dcc631e97c916460802f0a
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-11 19:06:47 +0200

    r

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,8 +11,16 @@ int **alloc_grid(int w, int h)
 	int i;
 	int **matr;
 
+	if (w <= 0 || h <= 0)
+		return (NULL);
 	matr = malloc(h * sizeof(int *));
+	if (matr == NULL)
+		return (NULL);
 	for (i = 0; i < h; i++)
+	{
 		matr[i] = malloc(w * sizeof(int));
+		if (matr[i] == NULL)
+			return (NULL);
+	}
 	return (matr);
 }

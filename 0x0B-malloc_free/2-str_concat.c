fe4b70f8dac47ce647c1a17518aaaaad36c38b78
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-11 13:13:02 +0200

    r

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,17 @@ char *str_concat(char *s1, char *s2)
 	char *cpy;
 
 	if (s1 == NULL)
-		s1[0] = 0;
+	{	
+		free(s1);
+		s1 = malloc(sizeof(char));
+		s1 = "";
+	}
 	else if (s2 == NULL)
-		s2[0] = 0;
+	{
+		free(s2);
+		s2 = malloc(sizeof(char));
+		s2 = "";
+	} 
 	/* puts("------1st waypoint------"); */
 	i = strlen(s1) + strlen(s2);
 	cpy = malloc((unsigned int) i);

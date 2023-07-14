96077fe59926bba9be141baa71a04de925b63186
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-14 22:21:03 +0200

    r

--- 0x0C-more_malloc_free/2-main.c
+++ 0x0C-more_malloc_free/2-main.c
@@ -10,6 +10,26 @@
  *
  * Return: Nothing.
  */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
 
 /**
  * main - check the code
@@ -23,5 +43,8 @@ int main(void)
     a = _calloc(98, sizeof(char));
     strcpy(a, "Best");
     strcpy(a + 4, " School! :)\n");
+    a[97] = '!';
+    simple_print_buffer(a, 98);
+    free(a);
     return (0);
 }

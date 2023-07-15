4486f5a77aef99d53437f83b41eae5fb03cb45c3
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-15 18:36:10 +0200

    r

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,6 +11,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *new;
 	unsigned int i = 0;
 
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	if (new_size <= old_size)
 		return (ptr);
 	new = malloc(new_size);

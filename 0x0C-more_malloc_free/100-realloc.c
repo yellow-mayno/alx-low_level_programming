0bdec3fe19db8c4545a126f3f8742257d9eb41f3
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-15 18:10:09 +0200

    r

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+/**
+* _realloc - sdfsdf
+* @ptr: dfgdfg
+* @old_size: sdf
+* @new_size: sdf
+* Return: sdfsdf
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new;
+	int i = 0;
+	
+	new = malloc(new_size * sizeof(*ptr));
+	if (new == NULL)
+		return (NULL);
+	for (; i < old_size; i++)
+		new[i] = ptr[i];
+	free(ptr);
+	return (new);
+}
+
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,3 +2,4 @@ void *mallox_checked(unsigned int);
 char *string_nconcat(char *, char *, unsigned int);
 void *_calloc(unsigned int, unsigned int);
 int *array_range(int, int);
+void *_realloc(void *, unsigned int, unsigned int);

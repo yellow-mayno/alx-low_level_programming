3032311fb8b8a6ffc1a8be99446427f89aaf4726
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-18 00:22:55 +0200

    r

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - fdsfsdf
+* @name: dsfgdsfg
+* @age: dfgdfg
+* @owner: dfgdfg
+*
+* Description: dfgdsdfs dsfsdfs
+*/
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+#endif

d631f52e2a9af40c347dce86184fcfb16e3d99ca
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-19 19:03:45 +0200

    r

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+/**
+* array_iterator - sdfsdf
+* @array: sdfsdf
+* @size: sdfsdf
+* @action: sdfsdf
+* Return: sdfsd
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i,
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}

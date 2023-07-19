f21b2a21637b1bd1d76bde9dfc0a9388617d63bf
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-19 20:58:28 +0200

    r

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+/**
+* int_index - sdfsfd
+* @array: sdfsd
+* @size: sdfsd
+* @cmp: sdfsdf
+* Return: sdfsdf
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, res = -1;
+
+	if (array == NULL || cmp == NULL)
+		return (res);
+	if (size <= 0)
+		return (res);
+	for (i = 0; !cmp(array[i]) && i < size; i++)
+		;
+	return (res);
+}

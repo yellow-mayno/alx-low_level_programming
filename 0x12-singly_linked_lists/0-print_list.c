9c8e456eb539bc57641f535aa884ff9b42c25625
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-28 15:38:07 +0200

    auto commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+* print_list - prints a list
+* @h: a list_t
+* Return: number of printed chars
+*/
+size_t print_list(const list_t *h)
+{
+    unsigned int cnt = 0;
+    list_t *ptr = (list_t *)h;
+
+    while (ptr != NULL)
+    {
+        if (ptr->str == NULL)
+            puts("[0] (nill)");
+        else
+            printf("[%u] %s\n", (*ptr).len, ptr->str);
+        cnt++;
+        ptr = ptr->next;
+    }
+    return (cnt);
+}

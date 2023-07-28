4601ed8acde78342b271e57c8862dc2610f21cc3
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-07-28 23:42:24 +0200

    auto commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,17 @@
 */
 size_t print_list(const list_t *h)
 {
-    unsigned int cnt = 0;
-    list_t *ptr = (list_t *)h;
+	unsigned int cnt = 0;
+	list_t *ptr = (list_t *)h;
 
-    while (ptr != NULL)
-    {
-        if (ptr->str == NULL)
-            puts("[0] (nill)");
-        else
-            printf("[%u] %s\n", (*ptr).len, ptr->str);
-        cnt++;
-        ptr = ptr->next;
-    }
-    return (cnt);
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+			puts("[0] (nill)");
+		else
+			printf("[%u] %s\n", (*ptr).len, ptr->str);
+		cnt++;
+		ptr = ptr->next;
+	}
+	return (cnt);
 }

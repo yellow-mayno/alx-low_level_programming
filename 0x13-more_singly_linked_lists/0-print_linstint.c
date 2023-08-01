0d38c308ea26b5b03af385eb0d9ba60778e2d1f9
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-08-01 18:58:49 +0200

    auto commit

---
+++ 0x13-more_singly_linked_lists/0-print_linstint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* print_listint - print ints of a sing linked list
+* @h: the pointer to first element
+* Return: the number of ints
+*/
+size_t print_listint(const listint_t *h)
+{
+	int cnt = 0;
+	listint_t *ptr;
+	
+	ptr = (listint_t *)h;
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		cnt++;
+		ptr = ptr->next;
+	}
+	return (cnt);
+}

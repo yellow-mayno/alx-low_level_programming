1c1862e8e00d9089069bbbe60714f42875bd9a0c
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-08-05 22:10:31 +0200

    auto commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 	while (b[i] != '\0')
 	{
 		if (b[i] == '0' || b[i] == '1')
-			cml = (cml + (b[i] - 48)) * 2;
+			cml = cml * 2 + (b[i] - 48);
 		else
 			return (0);
 		i++;

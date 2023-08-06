d56e6856374e80c27666c427a6081f6c1ea1a17d
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-08-06 18:14:04 +0200

    auto commit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,4 +4,5 @@ unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 void print_binary(unsigned long int);
 int get_bit(unsigned long int n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index);
 #endif

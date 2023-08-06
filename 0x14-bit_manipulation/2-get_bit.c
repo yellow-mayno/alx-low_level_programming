5b1394258184a0178be870eae7ae064c2c12686d
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-08-06 18:21:09 +0200

    auto commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,14 +1,13 @@
 #include <stddef.h>
 /**
-* set_bit - get the n-th bit starting from the lowest
+* get_bit - get the n-th bit starting from the lowest
 * @n: unsigned int
 * @index: the index ,??? xD
 * Return: the bit
 */
-int set_bit(unsigned long int *n, unsigned int index)
+int set_bit(unsigned long int n, unsigned int index)
 {
 	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
-	*n = *n | (1 << index);
-	return (1);
+	return ((n >> index) & 1);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,13 +1,14 @@
 #include <stddef.h>
 /**
-* get_bit - get the n-th bit starting from the lowest
+* set_bit - get the n-th bit starting from the lowest
 * @n: unsigned int
 * @index: the index ,??? xD
 * Return: the bit
 */
-int set_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
-	return ((n >> index) & 1);
+	*n = *n | (1 << index);
+	return (1);
 }

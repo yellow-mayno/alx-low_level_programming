9d5bfa840238d901b52180edaa47408fb6a0ee18
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-08-06 18:00:34 +0200

    auto commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,7 +7,7 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > sizeof(unisgned long int))
+	if (index > sizeof(unsigned long int))
 		return (-1);
 	return ((n >> index) & 1);
 }

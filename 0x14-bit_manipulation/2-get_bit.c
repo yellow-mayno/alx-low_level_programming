0a4a841a47a727ff4204fa9ec649a6f86173ad04
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-08-06 17:59:00 +0200

    auto commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 /**
 * get_bit - get the n-th bit starting from the lowest
 * @n: unsigned int
@@ -6,5 +7,7 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > sizeof(unisgned long int))
+		return (-1);
 	return ((n >> index) & 1);
 }

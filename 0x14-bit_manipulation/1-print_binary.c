73f00f7fadee375eb5bfa3d320737e04e8a330d2
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-08-06 16:15:09 +0200

    auto commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_binary - print an uint
+* @n: an unsigned int
+* Return: void
+*/
+void print_binary(unsigned int n)
+{
+	int i, start = 0;
+
+	i = sizeof(unsigned int) * 8 - 1;
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		for (; i >= 0; i--)
+		{
+			if (n >> i != 0)
+				start = 1;
+			if (start == 1)
+				_putchar('0' + ((n >> i) & 1));
+		}
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,4 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+void print_binary(unsigned int);
 #endif

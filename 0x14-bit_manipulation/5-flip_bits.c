01c08707936440ea9160a6b9bda04ccc83e90cd9
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-08-07 12:40:47 +0200

    auto commit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+/**
+* flip_bits - count the number of bits to be fliped
+* @n: the first number
+* @m: the second number
+* Return: the number of differences between the two strings of bits
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int cnt = 0, i = 0;
+	unsigned long int only_different;
+
+	only_different = (~n & m) | (n & ~m);
+	for (; i < sizeof(unsigned long int); i++)
+		cnt += (only_different >> i) & 1;
+	return (cnt);
+}
---
+++ 0x14-bit_manipulation/5main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	int i;
+
+	i = (int)flip_bits(8, 4)
+;	printf("%d\n", i);
+	return (0);
+}

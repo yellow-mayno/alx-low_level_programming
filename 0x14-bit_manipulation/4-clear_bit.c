3f7402c525a22e203cd5f58429b9e55be613e284
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-08-07 11:46:06 +0200

    auto commit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,5 +1,18 @@
-#include "main.h"
-#include <stdio.h>
+#include <stddef.h>
+/**
+* set_bit - get the n-th bit starting from the lowest
+* @n: unsigned int
+* @index: the index ,??? xD
+* Return: the bit
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	*n = *n | (1 << index);
+	return (1);
+}
+
 /**
 * clar_bit - set bit at index to 0
 * @n: the adress of the unsigned int

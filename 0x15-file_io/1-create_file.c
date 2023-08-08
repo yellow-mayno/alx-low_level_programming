ed008cb66176a293e61e52c9ce5697ba33319ead
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-08-09 01:36:41 +0200

    auto commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,9 +10,16 @@ int create_file(const char *filename, char *text)
 {
 	int r, fd, i = 0;
 
+	if (filename == NULL)
+		return (-1);
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 		return (-1);
+	if (text == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	for (; text[i] != '\0'; i++)
 		;
 	r = write(fd, text, i);

7ddbfe04b47b75b6e6fef7b67ab34d8fd46adc3b
dsdfsfd <dsjfskjdhfjsdf@sdfjhskdjfhskjdhf.com>
2023-08-09 00:58:54 +0200

    auto commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int create_file(const char *filename, char *text)
 {
 	int r, fd, i = 0;
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 		return (-1);
 	for (; text[i] != '\0'; i++)
